11ed5fd38596b41bfccc6e88a2e2ffe4eddafcc0
RickCaleg <59099175+RickCaleg@users.noreply.github.com>
2020-05-13 01:34:37 -0300

    Remove Force HTTPS

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 const app = require('./app/app');
 
-//HTTPS
-app.use(function (req, res, next) {
-    if ((req.get('X-Forwarded-Proto') !== 'https')) {
-        res.redirect('https://' + req.get('Host') + req.url);
-    } else
-        next();
-});
-
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
     console.log('O servidor está escutando a porta 3000');
